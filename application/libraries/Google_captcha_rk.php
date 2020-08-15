7f569a0ccb9e80119a02a23bc718443bcb8e2d99
Rachmad Kurniawan <rachmadkurniawandev@gmail.com>
2020-08-15 09:36:35 +0800

    fix: indentation in google captca rk

--- application/libraries/Google_captcha_rk.php
+++ application/libraries/Google_captcha_rk.php
@@ -29,20 +29,20 @@ class Google_captcha_rk
 
 	function validationCaptcha($captcha)
 	{
-        $credential = array(
-              'secret' => $this->secretKey,
-              'response' => $captcha
-          );
-        $verify = curl_init();
-        curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-        curl_setopt($verify, CURLOPT_POST, true);
-        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($credential));
-        curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($verify);
-
-        $status= json_decode($response, true);
-
+		$credential = array(
+			'secret' => $this->secretKey,
+			'response' => $captcha
+		);
+		$verify = curl_init();
+		curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+		curl_setopt($verify, CURLOPT_POST, true);
+		curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($credential));
+		curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($verify);
+		
+		$status= json_decode($response, true);
+		
 		return $status;
 	}
 }
